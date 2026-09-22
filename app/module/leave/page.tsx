61de2ec344c09319d535bf6e05e981b89c19eeab
jacobevci-lab <jacob.evci@gmail.com>
2026-09-22 18:53:24 +0300

    feat: add resilient live leave route

---
+++ app/module/leave/page.tsx
@@ -0,0 +1,8 @@
+import { WorkPayModulePage } from "@/components/work-pay-module-page";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default async function LeavePage() {
+  return WorkPayModulePage({ slug: "leave" });
+}
